61509fd4a79b488d108da6bbcd73b5968beaf3a0
graytrann <trankhaiphong987@gmail.com>
2024-05-06 16:24:45 +0700

    just push

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import "./App.css";
+import AuthButtons from "./components/AuthButtons";
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import Trending from "./components/Trending";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="grid  md:grid-cols-5">
+      <Nav />
+      <main className=" px-12 py-6 md:col-span-4 bg-cyan-50">
+        <AuthButtons />
+        <Header />
+        <Trending />
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Trending/index.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import MovieCard from "./MovieCard";
+import { moviesData } from "./moviesData";
+
+const Index = () => {
+  return (
+    <>
+      <h3 className="border-b border-primary mt-12 mb-6 pb-4">Trending</h3>
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-10 mb-12">
+        {moviesData.map((movie, index) => (
+          <MovieCard key={index} movie={movie}></MovieCard>
+        ))}
+      </div>
+      <div className="flex justify-center">
+        <button className="btn hover:scale-125 transition ease-out duration-500">
+          Load More
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default Index;
